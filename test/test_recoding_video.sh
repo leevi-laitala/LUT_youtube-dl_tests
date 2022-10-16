80d1eff72765e44e3a22ec02b5a1023e0917ac3c
Leevi Laitala <leevi.laitala@protonmail.com>
2022-10-16 21:58:03 +0300

    Update test_recoding_video.sh
    Fixed ffmpeg check. Running only ffmpeg will exit with code 1, so the check would always fail.
    
    By running it with -version flag, will exit with code 0 if installed.

--- test/test_recoding_video.sh
+++ test/test_recoding_video.sh
@@ -2,7 +2,7 @@
 [ -z "$YTEXEC" ] && { tput setaf 1; echo "ERROR: No YTEXEC environment variable defined." && exit 1 ; }
 VALUE=1
 #need ffmpeg
-ffmpeg &> /dev/null || { tput setaf 1; echo "ERROR: No ffmpeg installed. Skipping ..." && exit 2 ; }
+ffmpeg -version &> /dev/null || { tput setaf 1; echo "ERROR: No ffmpeg installed. Skipping ..." && exit 2 ; }
 mkdir videofolder
 cd ./videofolder
 
